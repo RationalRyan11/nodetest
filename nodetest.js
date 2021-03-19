6106e1867358e672c94104b7ce82aa1dc4f0ec61
Ryan <mailforabe7@yahoo.com>
2021-03-19 16:13:27 +0200

    First commit.

--- nodetest.js
+++ nodetest.js
@@ -5,19 +5,13 @@ const router = express.Router();
 
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
-router.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/about.html'));
-});
 
-router.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/sitemap.html'));
-});
-
-//add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
+
+//Julian's version maybe
+
